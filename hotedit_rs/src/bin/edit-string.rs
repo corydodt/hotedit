38aa0ee10b3a48552ba60ea81b4db18d2d76d4fc
Cory Dodt <121705+corydodt@users.noreply.github.com>
2022-04-26 05:40:39 +0000

    (#2) oops exit rc=1 on error

--- hotedit_rs/src/bin/edit-string.rs
+++ hotedit_rs/src/bin/edit-string.rs
@@ -5,7 +5,7 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         println!("** not enough arguments");
-        process::exit(0);
+        process::exit(1);
     }
     let hehe = HotEdit::new(&args[1]);
     match hehe.invoke() {
@@ -16,7 +16,7 @@ fn main() {
         }
         Err(e) => {
             println!("** bad edit: {}", e);
-            process::exit(0);
+            process::exit(1);
         }
     }
 }
